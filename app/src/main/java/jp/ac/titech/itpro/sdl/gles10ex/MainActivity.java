aa9e337091a5c992145d92fd949e1fa6f34fce6e
MikeCAT <mikecat@users.noreply.github.com>
2017-05-29 16:22:19 +0900

    add cat to screen

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         renderer = new SimpleRenderer();
         renderer.addObj(new Cube(0.5f, 0, 0.2f, -3));
         renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
+        renderer.addObj(new Cat(0.5f, -0.5f, -0.5f, 1));
         glView.setRenderer(renderer);
     }
 
